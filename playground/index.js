30b9dfe0dc9df2624e56b83ab181c62dc4716d05
kstodolak <kapi199726@gmail.com>
2021-10-12 22:38:17 +0200

    playground - usage of oRender added

--- playground/index.js
+++ playground/index.js
@@ -1,9 +1,9 @@
-import o, { oFragment } from '../src/o';
+import o, { oFragment, oRender } from '../src/o';
 
 
 const html = o('div').add(
     o('p').text('p - init').init(),
-    o('p').text('p - withot init'),
+    o('p').text('p - without init'),
     oFragment(
         o('p').text('p - inside oFragment with init').init()
     ),
@@ -13,8 +13,61 @@ const html = o('div').add(
     oFragment([
         o('p').text('oFragment with init')
     ]).init()
-).init();
-
-document.body.appendChild(
-    html
 );
+
+// oRender - Scenario #1
+oRender(
+    document.body,
+    html.init()
+);
+
+// oRender - Scenario #2
+// oRender(
+//     document.body,
+//     html
+// );
+
+// oRender - Scenario #3
+// const wrapper = o('div');
+// oRender(
+//     wrapper,
+//     html
+// );
+// check results:
+// console.log(wrapper.init());
+// or
+// document.body.appendChild(wrapper.init());
+// or
+// oRender(
+//     document.body,
+//     wrapper.init()
+// );
+
+// oRender - Scenario #4
+// const fragment = oFragment(
+//     html
+// );
+// oRender(
+//     document.body,
+//     fragment
+// );
+
+// oRender - Scenario #5
+// const fragment = oFragment(
+//     html.init()
+// );
+
+// oRender(
+//     document.body,
+//     fragment
+// );
+
+// oRender - Scenario #6
+// const fragment = oFragment(
+//     html.init()
+// );
+
+// oRender(
+//     document.body,
+//     fragment.init()
+// );
