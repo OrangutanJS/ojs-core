b8f5190e623d437173e571b2be83326075c7a037
kstodolak <kapi199726@gmail.com>
2021-11-22 19:47:38 +0100

    added third argument to oRender method - cleanParentContent

--- src/o.js
+++ src/o.js
@@ -206,7 +206,7 @@ export function oRef() {
 }
 
 
-export function oRender(parentNode, childNode) {
+export function oRender(parentNode, childNode, cleanParentContent = false) {
     if (Array.isArray(childNode)) {
         childNode.forEach(child => oRender(parentNode, child));
         return;
@@ -220,18 +220,20 @@ export function oRender(parentNode, childNode) {
     let parentNodeHTML = (parentNode instanceof HTMLElement) ? parentNode : parentNode.element;
     const renderNode = childNode => {
         if (childNode instanceof HTMLElement) {
+            if(cleanParentContent) parentNodeHTML.innerHTML = '';
             parentNodeHTML.appendChild(childNode);
             return;
         }
         if (childNode.__proto__.init) {
+            if(cleanParentContent) parentNodeHTML.innerHTML = '';
             parentNodeHTML.appendChild(childNode.init());
-        } 
+        }
     }
 
     if (childNode instanceof oFragment) {
         childNode.init().forEach(child => renderNode(child));
         return;
     }
-    
+
     renderNode(childNode);
 }
